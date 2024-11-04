0398c839ee232c892273b8280a4319f8d2015f43
Muhammad Ali <muhammadaliayaan295@gmail.com>
2024-11-04 20:35:54 +0500

    7kyu problem 2

---
+++ src/7Kyu/p2-Mubling/solution.js
@@ -0,0 +1,18 @@
+Description:
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+// URL: https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/javascript
+function accum(s) {
+    return s
+      .split('')
+      .map((char, index) => {
+        return char.toUpperCase() + char.toLowerCase().repeat(index);
+      })
+      .join('-');
+  }
