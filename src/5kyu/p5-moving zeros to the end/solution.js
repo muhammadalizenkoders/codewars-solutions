3aa881486a1e15a1930efa5536b5571b47cd499b
Muhammad Ali <muhammadaliayaan295@gmail.com>
2024-11-11 15:24:07 +0500

    5kyu problem 5

---
+++ src/5kyu/p5-moving zeros to the end/solution.js
@@ -0,0 +1,24 @@
+// Description:
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+// URL:https://www.codewars.com/kata/52597aa56021e91c93000cb0/javascript
+function moveZeros(arr) {
+    const n = arr.length;
+      const temp = new Array(n);
+  
+      let j = 0;
+  
+      for (let i = 0; i < n; i++) {
+          if (arr[i] !== 0) {
+              temp[j++] = arr[i];
+          }
+      }
+  
+      while (j < n)
+          temp[j++] = 0;
+  
+      for (let i = 0; i < n; i++)
+          arr[i] = temp[i];
+    return temp
+  }
